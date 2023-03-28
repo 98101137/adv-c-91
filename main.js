30b079536ab071a33cadb1eefc82efc8682b8b3b
98101137 <102752157+98101137@users.noreply.github.com>
2023-03-28 20:50:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function addUser(){
+    player_1 = document.getElementById("Player1_name_input").value;
+    player_2 = document.getElementById("player2_name_input").value;
+
+    localStorage.setItem("player_1",player_1);
+    localStorage.setItem("player_2",player_2);
+
+    window.location="index_game_page.html";
+    }
+    
